75f20fb632a051c120d422db189a6cfbe591bbdc
Harrison Mauseth <arthur.h.mauseth@gmail.com>
2024-04-26 21:49:37 -0500

    reorganization of project folder structure to clarify server-side, arduino, and misc files

---
+++ server-side/pet-food-tracker-server/src/main/java/com/harrisonmauseth/petfoodtracker/PetFoodTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.harrisonmauseth.petfoodtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetFoodTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetFoodTrackerApplication.class, args);
+	}
+
+}
---
+++ server-side/pet-food-tracker-server/src/test/java/com/harrisonmauseth/petfoodtracker/PetFoodTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harrisonmauseth.petfoodtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetFoodTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
