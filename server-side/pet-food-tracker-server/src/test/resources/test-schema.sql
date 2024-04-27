426830332d7139eb06589f1eb12f1140a97ceda6
Harrison Mauseth <arthur.h.mauseth@gmail.com>
2024-04-27 11:08:38 -0500

    added pet and tracker tables to test-schema

--- server-side/pet-food-tracker-server/src/test/resources/test-schema.sql
+++ server-side/pet-food-tracker-server/src/test/resources/test-schema.sql
@@ -10,4 +10,30 @@ CREATE TABLE users (
     CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
+CREATE TABLE pet (
+    pet_id SERIAL,
+    user_id int NOT NULL,
+    pet_name varchar(50) NOT NULL,
+    pet_nickname varchar(8) NULL,
+    pet_type varchar(20) NOT NULL,
+    pet_birthday timestamp NULL,
+    notes text NULL,
+    CONSTRAINT PK_pet PRIMARY KEY (pet_id),
+    CONSTRAINT FK_pet_users FOREIGN KEY (user_id) REFERENCES users
+);
+
+CREATE TABLE tracker (
+    tracker_id SERIAL,
+    user_id int NOT NULL,
+    pet_id int NOT NULL,
+    time_fed timestamp NOT NULL DEFAULT NOW(),
+    food_type varchar(255) NULL,
+    portion_amount numeric(4, 3) NULL,
+    portion_units varchar(20) NULL,
+    notes text NULL,
+    CONSTRAINT PK_tracker PRIMARY KEY (tracker_id),
+    CONSTRAINT FK_tracker_users FOREIGN KEY (user_id) REFERENCES users,
+    CONSTRAINT FK_tracker_pet FOREIGN KEY (pet_id) REFERENCES pet
+);
+
 COMMIT TRANSACTION;
