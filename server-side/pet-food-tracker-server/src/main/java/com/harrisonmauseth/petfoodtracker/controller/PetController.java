c1b7a1bd0882ed3a789409160e3d53b10f204ba3
Harrison Mauseth <arthur.h.mauseth@gmail.com>
2024-05-03 22:27:24 -0500

    implemented deletePet method

--- server-side/pet-food-tracker-server/src/main/java/com/harrisonmauseth/petfoodtracker/controller/PetController.java
+++ server-side/pet-food-tracker-server/src/main/java/com/harrisonmauseth/petfoodtracker/controller/PetController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,6 +62,12 @@ public class PetController {
                 throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Server error: " + e.getMessage());
             }
         }
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping(path = "/{id}")
+    public void deletePet(@PathVariable int id, Principal principal) {
+        petDao.deletePetByPetId(id, principal.getName());
     }
 
 }
